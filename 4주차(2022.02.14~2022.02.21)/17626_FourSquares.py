6dcea779b8e5c3ec61d731eab1a43ad5eefd5d4b
bgeun <papoke22@gmail.com>
2022-02-17 21:00:19 +0900

    2022.02.17

--- 4주차(2022.02.14~2022.02.21)/17626_FourSquares.py
+++ 4주차(2022.02.14~2022.02.21)/17626_FourSquares.py
@@ -1,8 +1,16 @@
-from math import sqrt
 import sys
 
-input = sys.stdin.readline
+N = int(sys.stdin.readline())
+dp = [0, 1]  # N = 0,1 일때 값 설정
 
-n = int(input())
+for i in range(2, N + 1):
+    min_value = 1e9  # 충분히 큰 수로 둠
+    j = 1
 
-print(int(sqrt(n)))
+    while (j**2) <= i:
+        min_value = min(min_value, dp[i - (j**2)])
+        j += 1
+    # i까지 같거나 작은 범위에서 j값을 1씩 증가시키면서 가장 큰 유효한 제곱수를 찾는다
+    dp.append(min_value + 1)
+
+print(dp[N])
