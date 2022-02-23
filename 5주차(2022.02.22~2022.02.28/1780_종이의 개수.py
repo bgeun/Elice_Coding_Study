dcb1a5ae7f7425c05a0a902686dce72f30c69e1b
bgeun <papoke22@gmail.com>
2022-02-23 17:45:54 +0900

    2022.02.23

---
+++ 5주차(2022.02.22~2022.02.28/1780_종이의 개수.py
@@ -0,0 +1,41 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+paper = [list(map(int, input().split())) for _ in range(N)]
+
+cnt = [0, 0, 0]  # -1, 0, 1
+
+
+def cut(x, y, N):
+    check = paper[x][y]
+    for i in range(x, x + N):
+        for j in range(y, y + N):
+            if check != paper[i][j]:  # 종이가 보두 같은 수 인지 검사
+                cut(x, y, N // 3)
+                cut(x, y + N // 3, N // 3)
+                cut(x, y + (N // 3 * 2), N // 3)
+                cut(x + N // 3, y, N // 3)
+                cut(x + N // 3, y + N // 3, N // 3)
+                cut(x + N // 3, y + (N // 3 * 2), N // 3)
+                cut(x + N // 3 * 2, y, N // 3)
+                cut(x + N // 3 * 2, y + N // 3, N // 3)
+                cut(x + N // 3 * 2, y + N // 3 * 2, N // 3)
+                return
+
+    if check == -1:
+        cnt[0] += 1
+        return
+    elif check == 0:
+        cnt[1] += 1
+        return
+    else:
+        cnt[2] += 1
+        return
+
+
+cut(0, 0, N)
+
+for i in cnt:
+    print(i)
