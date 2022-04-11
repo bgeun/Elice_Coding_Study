2f0b2dd8cd919d4f3f4ba0b3483cf08a220e3fa9
bgeun <papoke22@gmail.com>
2022-04-11 11:24:02 +0900

    22.04.11

---
+++ 11주차(2022.04.05~2022.04.11)/수식 최대화.py
@@ -0,0 +1,63 @@
+from itertools import permutations
+
+
+def solution(expression):
+    answer = 0
+    sep_expression = []
+    tmp = ""
+    for i in range(len(expression)):
+        if expression[i].isdecimal():  # 숫자라면 tmp 에 계속 더해주고
+            tmp += expression[i]
+        else:  # 연산자가 나올경우 list 에 숫자와 연산자를 각각 추가 해준다.
+            sep_expression.append(tmp)
+            tmp = ""
+            sep_expression.append(expression[i])
+    else:
+        sep_expression.append(tmp)  # 마지막 숫자 추가
+
+    print(sep_expression)
+
+    operators = ["+", "-", "*"]
+    for ordered_operator in permutations(
+        operators, 3
+    ):  # 퍼뮤테이션 함수를 이용해 연산자 3가지에 대한 순열을 생성
+        copy_expression = sep_expression[:]  # 여러번 계산해야 하므로 복사해서 사용한다.
+        for operator in ordered_operator:
+            idx = 0  # 연산자를 하나씩 가져와서
+            while idx < len(copy_expression):  # 0 > 9
+                if copy_expression[idx] == operator:  # 연산해준다.
+                    if operator == "-":
+                        cal = int(copy_expression[idx - 1]) - int(
+                            copy_expression[idx + 1]
+                        )
+                    elif operator == "+":
+                        cal = int(copy_expression[idx - 1]) + int(
+                            copy_expression[idx + 1]
+                        )
+                    else:
+                        cal = int(copy_expression[idx - 1]) * int(
+                            copy_expression[idx + 1]
+                        )
+
+                    copy_expression = (
+                        copy_expression[: idx - 1]
+                        + list(str(cal).split())
+                        + copy_expression[idx + 2 :]
+                    )  # 계산 한 후 계산값을 배열에 넣어준다.
+                    print(copy_expression, idx)
+                else:
+                    idx += 1
+
+        else:
+            answer = max(answer, abs(int(copy_expression[0])))  # 답은 절대값을 씌웠을 때 가장 큰 값
+
+    return answer
+
+
+# 1. 주어진 수식 숫자와 연산자로 분리
+# 2. permutation으로 연산자 3가지에 대한 순열 생성
+# 3. 생성된 순열마다 for문 돌면서 연산자 하나씩 가져와서 계산
+# 4. 계산할 때는 연산자를 기준으로 앞, 뒤 숫자 가져와서 계산
+#    4.1 만들어 놓은 수식 배열은 연산자 우선순위에 따라 여러 번 계산
+#         해야하므로 카피를 해서 사용
+#    4.2 앞, 뒤 숫자를 가져오기 위해 인덱스를 사용
--- 11주차(2022.04.05~2022.04.11)/프린터.py
+++ 11주차(2022.04.05~2022.04.11)/프린터.py
@@ -10,15 +10,14 @@ def solution(priorities, location):
     queue = deque(arr)
 
     while queue:
-        flag = 0
         x = queue.popleft()
         for i in range(0, len(queue)):
             if x[1] < queue[i][1]:
                 queue.append(x)
-                flag = 1
                 break
         # print(x)
-        if flag == 0:
+
+        else:
             cnt += 1
             if x[0] == location:
                 return cnt
